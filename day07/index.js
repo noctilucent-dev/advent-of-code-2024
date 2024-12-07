3870414bb892fb9d8d9544111daf9d8bfa68f950
Nathan Richards <nld@logios.co.uk>
2024-12-07 08:45:33 +0000

    Solved day 7 part 2

--- day07/index.js
+++ day07/index.js
@@ -17,22 +17,22 @@ function parse(lines) {
     return lines.map(l => l.split(/:?\s+/).map(Number));
 }
 
-function rCheck(target, total, remaining) {
-    if (remaining.length === 0) {
-        return total === target;
-    }
-    if (rCheck(target, total + remaining[0], remaining.slice(1))) {
-        return true;
-    }
-    if (rCheck(target, total * remaining[0], remaining.slice(1))) {
-        return true;
-    }
-    return false;
-}
-
 function part1(lines) {
     lines = parse(lines);
     log(lines);
+
+    const rCheck = (target, total, remaining) => {
+        if (remaining.length === 0) {
+            return total === target;
+        }
+        if (rCheck(target, total + remaining[0], remaining.slice(1))) {
+            return true;
+        }
+        if (rCheck(target, total * remaining[0], remaining.slice(1))) {
+            return true;
+        }
+        return false;
+    }
 
     lines = lines.filter(l => {
         const target = l[0];
@@ -42,6 +42,52 @@ function part1(lines) {
     return lines.map(l => l[0]).reduce((p,c) => p+c);
 }
 
+function part2(lines) {
+    lines = parse(lines);
+
+    const rCheck = (target, total, remaining) => {
+        if (remaining.length === 0) {
+            return total === target;
+        }
+        if (total > target) {
+            return false;
+        }
+
+        if (rCheck(target, total + remaining[0], remaining.slice(1))) {
+            return true;
+        }
+        if (rCheck(target, total * remaining[0], remaining.slice(1))) {
+            return true;
+        }
+        if (rCheck(target, Number(`${total}${remaining[0]}`), remaining.slice(1))) {
+            return true;
+        }
+        return false;
+    };
+
+    const timings = [];
+
+    lines = lines.filter(l => {
+        const target = l[0];
+        const nums = l.slice(1);
+
+        const startTime = performance.now();
+        let result = rCheck(target, 0, nums);
+        const endTime = performance.now();
+        
+        timings.push(endTime - startTime);
+        
+        return result;
+    });
+
+    const total = timings.reduce((p,c) => p+c);
+    const avg = total / timings.length;
+    log(`Total = ${total.toFixed(3)}ms, Average = ${avg.toFixed(3)}ms`);
+
+    return lines.map(l => l[0]).reduce((p,c) => p+c);
+}
+
 const lines = toTrimmedLines(raw);
 
 console.log(part1(lines));
+console.log(part2(lines));
