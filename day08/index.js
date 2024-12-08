c40ee45b4300315057cd4b0a49dae3bd1f372dbc
Nathan Richards <nld@logios.co.uk>
2024-12-08 16:43:34 +0000

    Solved day 8 part 2

--- day08/index.js
+++ day08/index.js
@@ -1,4 +1,4 @@
-const { DEBUG, log, getRaw, toTrimmedLines } = require('../util');
+const { DEBUG, log, getRaw, toTrimmedLines, gcd } = require('../util');
 
 let raw = getRaw();
 if (DEBUG) {
@@ -69,6 +69,60 @@ function part1(lines) {
     return unique.size;
 }
 
+function part2(lines) {
+    let {m, antennas} = parse(lines);
+    const maxY = m.length-1;
+    const maxX = m[0].length-1;
+    let frequencies = new Set(antennas.map(a => a.f));
+    antennas = Object.groupBy(antennas, a => a.f);
+    let antinodes = [];
+    frequencies.forEach(frequency => {
+        for(let i=0; i<antennas[frequency].length-1; i++) {
+            for(let j=i+1; j<antennas[frequency].length; j++) {
+                const a = antennas[frequency][i];
+                const b = antennas[frequency][j];
+
+                log(`Checking pair ${frequency} - (${a.x},${a.y}),(${b.x},${b.y})`);
+                //const v = [a.x - b.x, a.y-b.y];
+                const dy = a.y - b.y;
+                const dx = a.x - b.x;
+                const div = Math.abs(dx) / gcd(Math.abs(dy), Math.abs(dx));
+                log(`dx: ${dx}, dy: ${dy}, gcd: ${div}`);
+                log(`y = ${dy}(x - ${a.x}) / ${dx} + ${a.y}`);
+
+                // m = dy / dx
+                // y - y1  = m(x - x1)
+                // y - a.y = m(x - a.x)
+                // y = mx - m(a.x) + a.y
+                let calcY = (x) => ((dy*(x - a.x)) / dx) + a.y;
+                for(let x=0; x<=maxX; x += 1) {
+                    let z = dy*(x - a.x);
+                    if ((Math.abs(z) % Math.abs(dx)) !== 0) {
+                        log(`Skipping x=${x}`);
+                        continue;
+                    } else {
+                        log(`x=${x}, z=${z}`);
+                    }
+                    let y = calcY(x);
+                    if (y < 0 || y > maxY) continue;
+                    // if (Math.abs(y - Math.floor(y)) < 0.1) {
+                        log(`Found integer co-ordinate (${x},${y})`);
+                        antinodes.push({
+                            f: frequency,
+                            x,
+                            y
+                        });
+                    // }
+                }
+            }
+        } 
+    });
+    let unique = new Set(antinodes.map(({x,y}) => [x,y]).map(JSON.stringify));
+    log(unique);
+    return unique.size;
+}
+
 const lines = toTrimmedLines(raw);
 
 console.log(part1(lines));
+console.log(part2(lines)); // 996 too low, 1012 too low, 1234 not correct
